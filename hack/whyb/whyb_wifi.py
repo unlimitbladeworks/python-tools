0fb53acd4c8843f41025afbf78808802fa8afab5
unlimitbladeworks <13121347080@sina.cn>
2018-12-03 18:39:10 +0800

    add wifipy

---
+++ hack/whyb/whyb_wifi.py
@@ -0,0 +1,25 @@
+#coding = utf-8
+
+"""
+@author: sy
+
+@file: whyb_wifi.py
+
+@time: 2018/12/3 16:50
+
+@desc: 扫描windows主机的注册表，将注册表中wifi连接过的mac地址记录，并通过现有的在线数据库网址进行查询物理地址
+
+"""
+
+class WHYB():
+
+    # TODO 在注册表中分析无线热点
+    pass
+
+    # TODO 分析出的MAC地址传给wigle进行解析获取结果
+
+
+
+
+if __name__ == '__main__':
+    pass

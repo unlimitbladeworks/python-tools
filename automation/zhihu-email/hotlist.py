cccd8946c8ede31c698ed2de0469b1e9ef547b3e
unlimitbladeworks <13121347080@sina.cn>
2020-08-17 18:45:58 +0800

    知乎热榜完善代码

--- automation/zhihu-email/hotlist.py
+++ automation/zhihu-email/hotlist.py
@@ -34,7 +34,11 @@ def crawl_hot_list():
             for data in data_list:
                 target = data['target']
                 title = target['title'].encode("utf-8").decode("utf-8")  # 标题
-                url = target['url']  # 问题地址
+                # 问题地址,替换api和questions字段，得到真实地址
+                old_url = target['url']
+                if 'questions' not in old_url:
+                    continue
+                url = old_url.replace('api', 'www').replace('questions', 'question')
                 answer_count = target['answer_count']  # 回答数
                 follower_count = target['follower_count']  # 关注数
                 detail_text = data['detail_text'].encode("utf-8").decode("utf-8")  # 热度
@@ -46,12 +50,13 @@ def crawl_hot_list():
                     'detail_text': detail_text
                 }
                 hot_list.append(result_dict)
-        except Exception:
+        except:
             import traceback
             traceback.print_exc()
             continue
-        result_list = sort(hot_list)
-        txt = deal_txt(result_list)
+
+        result_list = sort(hot_list)  # 根据热度排序
+        txt = deal_txt(result_list)  # 处理文本
         mail.send_mails(txt)  # 发邮件
 
 

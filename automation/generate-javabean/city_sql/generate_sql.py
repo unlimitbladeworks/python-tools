9aee123aaf6b429b35f3f5cf978323c61389b43e
unlimitbladeworks <13121347080@sina.cn>
2021-09-22 14:04:28 +0800

    地区生成字典sql

--- automation/generate-javabean/city_sql/generate_sql.py
+++ automation/generate-javabean/city_sql/generate_sql.py
@@ -7,24 +7,27 @@
 """
 
 import pandas as pd
+import requests
 
 
 def read_excel():
     sql_str = "INSERT INTO T_DICT (DICT_CODE, DICT_NAME, DICT_VALUE, PARENT_DICT_CODE, DICT_REMARK) VALUES " \
-              "('PROVINCIAL_DICT', '省级字典', '', '', null);"
+              "('AREAS_DICT', '区级字典', '', '', null);"
     print(sql_str)
 
     """ 读取 Excel 模板内容 """
-    df = pd.read_csv('provinces.csv')
+    df = pd.read_csv('areas.csv')
     for index, row in df.iterrows():
-        code = row['code']
+        city_code = row['cityCode']
+        province_code = row['provinceCode']
         name = row['name']
-        generate_code(code, name)
+        code = row['code']
+        generate_code(code, name, province_code, city_code)
 
 
-def generate_code(code, name):
+def generate_code(code, name, province_code, city_code):
     sql_str = "INSERT INTO T_DICT (DICT_CODE, DICT_NAME, DICT_VALUE, PARENT_DICT_CODE, DICT_REMARK) VALUES " \
-              f"('{code}', '{name}', '', 'PROVINCIAL_DICT', null);"
+              f"('{code}', '{name}', '{city_code}', 'AREAS_DICT', '{province_code}');"
     print(sql_str)
 
 
